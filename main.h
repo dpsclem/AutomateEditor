a0753a229ae84832ab3d01c51c17b2899ccf0f73
Clément <cledupas@gmail.com>
2021-12-29 15:19:39 +0100

    added main.h

---
+++ main.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+
+#define true 1
+#define false 0
+
+
+char** readFileLines(char* path);
+char* ConcatWords(char *firstWord, char *secondWord);
+bool IsWordEmpty(char *wordToCheck);
+char *GetWordMirror(char *initialWord);
