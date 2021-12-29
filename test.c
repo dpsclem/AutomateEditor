3678e44bf2e369770ef494bb17e6af2852d6f5da
Clément <cledupas@gmail.com>
2021-12-29 13:16:37 +0100

    Removed commented code

--- test.c
+++ test.c
@@ -19,34 +19,3 @@ char* ConcatWords(char *firstWord, char *secondWord){
 
     return result;
 }
-
-
-
-// char** readFileLines(char* path) {
-
-//     char line[MAX_LINE_LENGTH] = {0};
-//     unsigned int line_count = 0;
-    
-    
-//     /* Open file */
-//     FILE *file = fopen(path, "r");
-    
-//     if (!file)
-//     {
-//         perror(path);
-//     }
-    
-//     while (fgets(line, MAX_LINE_LENGTH, file))
-//     {
-//         printf("line[%06d]: %s", ++line_count, line);
-        
-//         if (line[strlen(line) - 1] != '\n')
-//             printf("\n");
-//     }
-    
-//     if (fclose(file))
-//     {
-//         perror(path);
-//     }
-// }
-
